90f48b431495ffb42baa642774f09ae703aefef0
chuantiong <tiongchuan@gmail.com>
2024-04-27 22:53:40 +0800

    Add Attribution component

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,22 @@ import './App.scss';
 import Control from './components/Control';
 import { ValueProvider } from './contexts/ValueContext';
 import { RegexProvider } from './contexts/RegexContext';
+import Attribution from './components/Attribution';
 
 const App = () => {
 
   return (
-    <main className="main">
-      <h4 className="pg-text">password generator</h4>
-      <ValueProvider>
-        <RegexProvider>
-          <Control />
-        </RegexProvider>
-      </ValueProvider>
-    </main>
+    <>
+      <main className="main">
+        <h4 className="pg-text">password generator</h4>
+        <ValueProvider>
+          <RegexProvider>
+            <Control />
+          </RegexProvider>
+        </ValueProvider>
+      </main>
+      <Attribution />
+    </>
   )
 }
 
